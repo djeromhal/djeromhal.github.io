bf6097eb98b38312aa2ae6432a595c868addc986
djeromhal <ivankohw@mail.ru>
2015-07-24 03:40:07 +0300

    box3

--- box3/js/form.js
+++ box3/js/form.js
@@ -1,30 +1,15 @@
-﻿  // document.getElementById('feedback-form').onsubmit = function(){
-  //   var http = new XMLHttpRequest();
-  //   http.open("POST", "php/form.php", true);
-  //   http.setRequestHeader("Content-Type", "application/x-www-form-urlencoded");
-  //   http.send("nameF=" + $('#inputname').val() + "&contactF=" + $('#inputmail').val() + "&textF=" + $('#inputtext').val());
-  //   http.onreadystatechange = function() {
-  //     if (http.readyState == 4 && http.status == 200) {
-  //       alert(http.responseText + ', Ваше сообщение получено.\nНаши специалисты ответят Вам в ближайшее время.\nБлагодарим за проявленный интерес!');
-  //     }
-  //   }
-  //   http.onerror = function() {
-  //     alert('Извините, данные не были переданы');
-  //   };
-  //   return false;
-  // };
-  document.getElementById('feedback-form-GVR').onsubmit = function(){
-  var http = new XMLHttpRequest();
-  http.open("POST", "php/formGVR.php", true);
-  http.setRequestHeader("Content-Type", "application/x-www-form-urlencoded");
-  http.send("nameGVR=" + this.nameGVR.value + "&contactGVR=" + this.contactGVR.value + "&telGVR=" + this.telGVR.value);
-  http.onreadystatechange = function() {
-    if (http.readyState == 4 && http.status == 200) {
-      alert(http.responseText + ', Ваше сообщение получено.\nНаши специалисты ответят Вам в ближайшее время.\nБлагодарим за проявленный интерес!');
+﻿  document.getElementById('feedback-form').onsubmit = function(){
+    var http = new XMLHttpRequest();
+    http.open("POST", "php/form.php", true);
+    http.setRequestHeader("Content-Type", "application/x-www-form-urlencoded");
+    http.send("nameF=" + this.nameF.value + "&contactF=" + this.contactF.value + "&textF=" + this.textF.value);
+    http.onreadystatechange = function() {
+      if (http.readyState == 4 && http.status == 200) {
+        alert(http.responseText + ', Ваше сообщение получено.\nНаши специалисты ответят Вам в ближайшее время.\nБлагодарим за проявленный интерес!');
+      }
     }
-  }
-  http.onerror = function() {
-    alert('Извините, данные не были переданы');
+    http.onerror = function() {
+      alert('Извините, данные не были переданы');
+    };
+    return false;
   };
-  return false;
-};
