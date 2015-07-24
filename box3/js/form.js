585f7927e958d32cccac7753b78fe8266d31b011
djeromhal <ivankohw@mail.ru>
2015-07-24 03:32:25 +0300

    gvr box3

--- box3/js/form.js
+++ box3/js/form.js
@@ -1,15 +1,30 @@
-﻿  document.getElementById('feedback-form').onsubmit = function(){
-    var http = new XMLHttpRequest();
-    http.open("POST", "php/form.php", true);
-    http.setRequestHeader("Content-Type", "application/x-www-form-urlencoded");
-    http.send("nameF=" + $('#inputname').val() + "&contactF=" + $('#inputmail').val() + "&textF=" + $('#inputtext').val());
-    http.onreadystatechange = function() {
-      if (http.readyState == 4 && http.status == 200) {
-        alert(http.responseText + ', Ваше сообщение получено.\nНаши специалисты ответят Вам в ближайшее время.\nБлагодарим за проявленный интерес!');
-      }
+﻿  // document.getElementById('feedback-form').onsubmit = function(){
+  //   var http = new XMLHttpRequest();
+  //   http.open("POST", "php/form.php", true);
+  //   http.setRequestHeader("Content-Type", "application/x-www-form-urlencoded");
+  //   http.send("nameF=" + $('#inputname').val() + "&contactF=" + $('#inputmail').val() + "&textF=" + $('#inputtext').val());
+  //   http.onreadystatechange = function() {
+  //     if (http.readyState == 4 && http.status == 200) {
+  //       alert(http.responseText + ', Ваше сообщение получено.\nНаши специалисты ответят Вам в ближайшее время.\nБлагодарим за проявленный интерес!');
+  //     }
+  //   }
+  //   http.onerror = function() {
+  //     alert('Извините, данные не были переданы');
+  //   };
+  //   return false;
+  // };
+  document.getElementById('feedback-form-GVR').onsubmit = function(){
+  var http = new XMLHttpRequest();
+  http.open("POST", "php/formGVR.php", true);
+  http.setRequestHeader("Content-Type", "application/x-www-form-urlencoded");
+  http.send("nameGVR=" + this.nameGVR.value + "&contactGVR=" + this.contactGVR.value + "&telGVR=" + this.telGVR.value);
+  http.onreadystatechange = function() {
+    if (http.readyState == 4 && http.status == 200) {
+      alert(http.responseText + ', Ваше сообщение получено.\nНаши специалисты ответят Вам в ближайшее время.\nБлагодарим за проявленный интерес!');
     }
-    http.onerror = function() {
-      alert('Извините, данные не были переданы');
-    };
-    return false;
+  }
+  http.onerror = function() {
+    alert('Извините, данные не были переданы');
   };
+  return false;
+};
--- box3/php/form.php
+++ box3/php/form.php
@@ -1,8 +1,14 @@
 <?
-if (array_key_exists('textF', $_POST)) {
+// if (array_key_exists('textF', $_POST)) {
+//   mail ("ivankohw@mail.ru",
+//         "Заполнена форма ".$_SERVER['HTTP_REFERER'],
+//         "Имя: ".$_POST['nameF']."\nE-mail: ".$_POST['contactF']."\nСообщение: ".$_POST['textF']);
+//   echo $_POST['nameF'];
+// }
+if (array_key_exists('telGVR', $_POST)) {
   mail ("ivankohw@mail.ru",
-        "Заполнена форма ".$_SERVER['HTTP_REFERER'],
-        "Имя: ".$_POST['nameF']."\nE-mail: ".$_POST['contactF']."\nСообщение: ".$_POST['textF']);
-  echo $_POST['nameF'];
+        "Заполнена форма на оформление групповой визы с ".$_SERVER['HTTP_REFERER'],
+        "Имя руководителя: ".$_POST['nameGVR']."\nEmail: ".$_POST['contactGVR']."\nТелефон: ".$_POST['telGVR']);
+  echo $_POST['nameGVR'];
 }
 ?>
