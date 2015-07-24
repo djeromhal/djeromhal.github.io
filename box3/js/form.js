b5818c3dc17b22c26fb6045a80968e71a93e634a
djeromhal <ivankohw@mail.ru>
2015-07-24 03:24:55 +0300

    box3

--- box3/js/form.js
+++ box3/js/form.js
@@ -1,8 +1,8 @@
 ﻿  document.getElementById('feedback-form').onsubmit = function(){
     var http = new XMLHttpRequest();
-    http.open("POST", "../php/form.php", true);
+    http.open("POST", "php/form.php", true);
     http.setRequestHeader("Content-Type", "application/x-www-form-urlencoded");
-    http.send("nameF=" + this.nameF.value + "&contactF=" + this.contactF.value + "&textF=" + this.textF.value);
+    http.send("nameF=" + $('#inputname').val() + "&contactF=" + $('#inputmail').val() + "&textF=" + $('#inputtext').val());
     http.onreadystatechange = function() {
       if (http.readyState == 4 && http.status == 200) {
         alert(http.responseText + ', Ваше сообщение получено.\nНаши специалисты ответят Вам в ближайшее время.\nБлагодарим за проявленный интерес!');
