db4e905058dcb429f4c6175cbeaed224bb5de05f
asterixyoursite <mikhail@asterixyoursite.ru>
2017-12-02 17:39:03 +0300

    scroll

--- coworking/js/main.js
+++ coworking/js/main.js
@@ -909,11 +909,6 @@ $(function(){
 	   // }
 	   lastScrollTop = st;
 	})*/
-	// Reasonable defaults
-	var PIXEL_STEP  = 10;
-	var LINE_HEIGHT = 40;
-	var PAGE_HEIGHT = 800;
-
 	function normalizeWheelSpeed(event) {
 	    var normalized;
 	    if (event.wheelDelta) {
@@ -924,12 +919,58 @@ $(function(){
 	    }
 	    return normalized;
 	}
+	// Reasonable defaults
+var PIXEL_STEP  = 10;
+var LINE_HEIGHT = 40;
+var PAGE_HEIGHT = $(document).height();
+
+function normalizeWheel(/*object*/ event) /*object*/ {
+  var sX = 0, sY = 0,       // spinX, spinY
+      pX = 0, pY = 0;       // pixelX, pixelY
+
+  // Legacy
+  if ('detail'      in event) { sY = event.detail; }
+  if ('wheelDelta'  in event) { sY = -event.wheelDelta / 120; }
+  if ('wheelDeltaY' in event) { sY = -event.wheelDeltaY / 120; }
+  if ('wheelDeltaX' in event) { sX = -event.wheelDeltaX / 120; }
+
+  // side scrolling on FF with DOMMouseScroll
+  if ( 'axis' in event && event.axis === event.HORIZONTAL_AXIS ) {
+    sX = sY;
+    sY = 0;
+  }
+
+  pX = sX * PIXEL_STEP;
+  pY = sY * PIXEL_STEP;
+
+  if ('deltaY' in event) { pY = event.deltaY; }
+  if ('deltaX' in event) { pX = event.deltaX; }
+
+  if ((pX || pY) && event.deltaMode) {
+    if (event.deltaMode == 1) {          // delta in LINE units
+      pX *= LINE_HEIGHT;
+      pY *= LINE_HEIGHT;
+    } else {                             // delta in PAGE units
+      pX *= PAGE_HEIGHT;
+      pY *= PAGE_HEIGHT;
+    }
+  }
+
+  // Fall-back if spin cannot be determined
+  if (pX && !sX) { sX = (pX < 1) ? -1 : 1; }
+  if (pY && !sY) { sY = (pY < 1) ? -1 : 1; }
+
+  return { spinX  : sX,
+           spinY  : sY,
+           pixelX : pX,
+           pixelY : pY };
+}
 	var distance = 0;
 	var antidist = 0;
 	var neg = 1;
 	var down = true;
 	var handleScroll = function(event){
-		delta = normalizeWheelSpeed(event);
+		delta = -normalizeWheel(event).spinY;
 			var actDrag = $(this);
 		thisInnerItem = actDrag.find('.active_inner');
 		nextInnerItem = thisInnerItem.next('.next_inner_item');
@@ -945,8 +986,7 @@ $(function(){
 		if(!hasprev && delta > 0 && distance >= 0) return false;
 		if(!hasnext && delta < 0) return false;
 
-		distance += delta * 30;
-		console.log(distance)
+		distance += delta * 50;
 		// antidist += delta * 30 * neg;
 		thisInnerItem.css({transform: 'translateY(' + distance + 'px)'});
 		// if(delta < 0){
