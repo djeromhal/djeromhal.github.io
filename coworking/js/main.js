a918a53a370a8c9304ed6010d427cce47ca60dc0
asterixyoursite <mikhail@asterixyoursite.ru>
2017-12-04 14:13:25 +0300

    scroll

--- coworking/js/main.js
+++ coworking/js/main.js
@@ -1015,10 +1015,9 @@ $(function(){
 		aH = $('.active_drag').find('.active_inner');
 		aCH = aH.children('div');
 		if(aH.height() < aCH.height()){
-			noscroll = true;
-			aH.attr('style','');
 			if(aH.scrollTop() === 0 && delta < 0 || aH.scrollTop() + aH.outerHeight() === aCH.outerHeight() && delta > 0){
-				
+				noscroll = true;
+				aH.attr('style','');
 			}
 		}
 	}
