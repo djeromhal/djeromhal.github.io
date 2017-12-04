b25d20f2639d7eb61d017bc42a198a9ba3396a24
asterixyoursite <mikhail@asterixyoursite.ru>
2017-12-04 15:48:29 +0300

    scroll

--- coworking/js/main.js
+++ coworking/js/main.js
@@ -1098,22 +1098,22 @@ $(function(){
 			if(timer){
 				if(Math.abs(distance) > thisInnerItem.height() / 2){
 					if(distance > 0){
-						thisInnerItem.addClass('tyb').delay(510).queue(function(next){
+						thisInnerItem.addClass('tyb').delay(310).queue(function(next){
 							$(this).attr('style','').removeClass('tyb active_inner').addClass('next_inner_item');
 							next();
 						})
-						prevInnerItem.addClass('tyb').delay(510).queue(function(next){
+						prevInnerItem.addClass('tyb').delay(310).queue(function(next){
 							$(this).attr('style','').removeClass('tyb prev_inner_item').addClass('active_inner').prev().addClass('prev_inner_item');
 							checkForContinue();
 							updateVNav();
 							next();
 						})
 					}else{
-						thisInnerItem.addClass('tyt').delay(510).queue(function(next){
+						thisInnerItem.addClass('tyt').delay(310).queue(function(next){
 							$(this).attr('style','').removeClass('tyt active_inner').addClass('prev_inner_item');
 							next();
 						})
-						nextInnerItem.addClass('tyt').delay(510).queue(function(next){
+						nextInnerItem.addClass('tyt').delay(310).queue(function(next){
 							$(this).attr('style','').removeClass('tyt next_inner_item').addClass('active_inner').next().addClass('next_inner_item');
 							checkForContinue();
 							updateVNav();
@@ -1121,15 +1121,15 @@ $(function(){
 						})
 					}
 				}else{
-					thisInnerItem.addClass('ty0').delay(510).queue(function(next){
+					thisInnerItem.addClass('ty0').delay(310).queue(function(next){
 						$(this).attr('style','').removeClass('ty0');
 						next();
 					})
-					prevInnerItem.addClass('ty0').delay(510).queue(function(next){
+					prevInnerItem.addClass('ty0').delay(310).queue(function(next){
 						$(this).attr('style','').removeClass('ty0');
 						next();
 					})
-					nextInnerItem.addClass('ty0').delay(510).queue(function(next){
+					nextInnerItem.addClass('ty0').delay(310).queue(function(next){
 						$(this).attr('style','').removeClass('ty0');
 						delta = delta > 0 ? -Math.abs(delta) : Math.abs(delta);
 						checkForContinue();
@@ -1138,7 +1138,7 @@ $(function(){
 				}
 				distance = 0;
 			}
-		}, 800));
+		}, 500));
 		// ajax load
 		if(ajaxloader_flag && isAttr(nextInnerItem) && delta < 0 || ajaxloader_flag && isAttr(prevInnerItem) && delta > 0){
 			var nextToAnim;
