a5d07a7f0fd3595c6dd20a14ad3f4e7abb59b6f1
Mikhail Ivanko <mikhailivanko@gmail.com>
2019-04-22 23:43:36 +0300

    wdd 56

--- wdd/js/main.js
+++ wdd/js/main.js
@@ -26,19 +26,11 @@ $(function(){
 		// 	// alert()
 		// })
 	}
-	
-	$('.main-menu-options .option-anim').on('click',function(e){
+	$('.option-anim, .top-nav-links-a').on('click',function(e){
 		setTimeout(function(){
 			$('audio').trigger('pause');
 			$('audio').prop("currentTime",0);
 			$('#click-audio').trigger('play');
-		},0)
-	})
-	$('.main-menu-options .option-anim').on('mouseenter',function(e){
-		setTimeout(function(){
-			$('audio').trigger('pause');
-			$('audio').prop("currentTime",0);
-			$('#hover-audio').trigger('play');
 		},0)
 	})
 	$('.option-anim').on('click',function(e){
@@ -61,15 +53,20 @@ $(function(){
 			}
 		},500)
 	})
-	// $('.option-anim').on('mouseenter', function(e){
-	// 	// var media = $(this).find('audio')[0];
-	// 	// media.load();
-	// 	// const playPromise = media.play();
-	// 	// if (playPromise !== null){
-	// 	//     playPromise.catch(() => { media.play(); })
-	// 	// }
+	$('.option-anim, .top-nav-links-a').on('mouseenter', function(e){
+		// var media = $(this).find('audio')[0];
+		// media.load();
+		// const playPromise = media.play();
+		// if (playPromise !== null){
+		//     playPromise.catch(() => { media.play(); })
+		// }
+		setTimeout(function(){
+			$('audio').trigger('pause');
+			$('audio').prop("currentTime",0);
+			$('#hover-audio').trigger('play');
+		},0)
 
-	// })
+	})
 	// Content scaling
 	var maxWidth  = 1920;
 	var maxHeight = 1080;
