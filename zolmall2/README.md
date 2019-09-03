55a2aae77c71f6dd74c2d84c6de8711a3073b4d6
djeromhal <mikhailivanko@gmail.com>
2019-09-03 17:39:57 +0300

    zolmall2

---
+++ zolmall2/README.md
@@ -0,0 +1,45 @@
+Gulp build system with PostCSS, Sass, UglifyJS, Browsersync
+========================================================
+
+> Automating front-end build processes with CSS/SASS/JS import and merge
+
+## Install
+### Requirements
+
+Node (use brew or install it from [here](http://nodejs.org/download/))
+
+```bash
+brew install node
+```
+
+### Clone this repository
+
+```bash
+git clone https://github.com/czombos/gulp-postcss-sass-starter-kit.git
+```
+
+### Install npm dependencies
+
+```bash
+npm install
+```
+
+### List of available tasks
+#### `gulp css`
+Compile Scss to CSS and combine imported CSS files
+
+#### `gulp js`
+Combine JS files & uglifies the output
+
+#### `gulp serve`
+Start a new server and watch for CSS & JS & HTL file changes
+
+#### `gulp watch`
+Watches for CSS & JS & HTL file changes
+
+#### `gulp`
+Run the following tasks simultaneously: `gulp css`, `gulp js`, `gulp serve` & `gulp watch`
+
+-------------------------------------
+
+Visit my website: https://siteanalyzerpro.com/
---
+++ zolmall2/gulpfile.js
@@ -0,0 +1,57 @@
+var gulp = require('gulp'),
+	rename = require("gulp-rename"),
+	uglify = require('gulp-uglify'),
+	include = require("gulp-include"),
+	browserSync = require('browser-sync'),
+	reload = browserSync.reload;
+
+var postcss = require('gulp-postcss'),
+	sass = require('gulp-sass'),
+	cssImport = require('postcss-import'),
+	autoprefixer = require('autoprefixer'),
+	cssnext = require('gulp-cssnext'),
+	mqpacker = require('css-mqpacker'),
+	cssnano = require('cssnano'),
+	sourcemaps = require('gulp-sourcemaps'),
+	pixrem = require('pixrem');
+
+gulp.task('css', function(){
+	var processors = [
+		cssImport,
+		autoprefixer({browsers: ['last 2 version', 'safari 5', 'ie 8', 'ie 9', 'opera 12.1', 'ios 6', 'android 4']}),
+		mqpacker,
+		pixrem,
+		cssnano({"discardComments": {"removeAll": true}})
+	];
+	return gulp.src('./sass/style.scss')
+		.pipe(sass().on('error', sass.logError))
+		.pipe(cssnext({compress: true }))
+		.pipe(postcss(processors))
+		.pipe(rename('style.min.css'))
+		.pipe(gulp.dest('./css'));
+});
+
+gulp.task('js', function(){
+	return gulp.src('./js/global.js')
+		.pipe(include())
+		.on('error', console.log)
+		.pipe(rename('global.min.js'))
+		.pipe(uglify())
+		.pipe(gulp.dest('./js'));
+});
+
+gulp.task('serve', function(){
+	browserSync({
+		server: {
+			baseDir: "./"
+		}
+	});
+});
+
+gulp.task('watch', function(){
+	gulp.watch('./sass/*.scss', ['css', browserSync.reload]);
+	gulp.watch('*.html', browserSync.reload);
+	// gulp.watch('./js/*.js', ['js', browserSync.reload]);
+});
+
+gulp.task('default', ['css', 'serve', 'js', 'watch']);
