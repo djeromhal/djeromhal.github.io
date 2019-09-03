6e94894e5d139a3e02c966cfe6a539583f13f5bc
djeromhal <mikhailivanko@gmail.com>
2019-09-03 17:59:06 +0300

    zolmall

--- pageonmillion/php/form.php
+++
@@ -1,8 +0,0 @@
-<?
-if (array_key_exists('contactF', $_POST)) {
-  mail ("ivankohw@mail.ru",
-        "Заполнена форма ".$_SERVER['HTTP_REFERER'],
-        "Имя: ".$_POST['nameF']."\nE-mail: ".$_POST['contactF']);
-  echo $_POST['nameF'];
-}
-?>
