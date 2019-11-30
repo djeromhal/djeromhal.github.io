5cefd85fb84e259a82e33c4352881876acff72c3
djeromhal <mikhailivanko@gmail.com>
2019-11-30 10:11:18 +0300

    Remove box3

--- box3/php/form.php
+++
@@ -1,8 +0,0 @@
-<?
-if (array_key_exists('textF', $_POST)) {
-  mail ("ivankohw@mail.ru",
-        "Заполнена форма ".$_SERVER['HTTP_REFERER'],
-        "Имя: ".$_POST['nameF']."\nE-mail: ".$_POST['contactF']."\nСообщение: ".$_POST['textF']);
-  echo $_POST['nameF'];
-}
-?>
